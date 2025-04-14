5535efff26be9551c28db052d5a52790c31e642f
aaronersando <agersando@gmail.com>
2025-04-15 04:34:12 +0800

    Error page done

--- src/main/resources/static/frontend/src/App.jsx
+++ src/main/resources/static/frontend/src/App.jsx
@@ -2,16 +2,22 @@ import Contactform from "./components/forms/Contactform";
 import Login from "./pages/auth/Login";
 import Contact from "./pages/public/Contact";
 import Home from "./pages/public/Home";
-
+import Error from "./pages/public/Error";
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   
 
   return (
     <>
+    <BrowserRouter>
+      <Error/>
+    </BrowserRouter>
       {/* <Login/> */}
       {/* <Contact/> */}
-      <Home/>
+      {/* <Home/> */}
+      
     </>
   )
 }
